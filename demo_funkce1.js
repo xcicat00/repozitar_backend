93a8dfe2f6b2e946aec874e4594e5882ce6f11d3
Ladislav Cicatko <cicatkolada@gmail.com>
2021-06-30 19:57:15 +0200

    pokus o zapis do kolekce pomoci insertOne()

---
+++ demo_funkce1.js
@@ -0,0 +1,13 @@
+/* deklarace finaln9ho callbacku
+tento se pouziva az na uplnem konci kodu funkce
+myFunction((callbackParam) => {
+console.log(callbackParam);
+}); */
+
+/* lepe posefovana predchozi varianta
+if (require.main) {
+myFunction((callbackParam) => {
+console.log(callbackParam);
+});
+}
+*/
