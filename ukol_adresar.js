9d67dc7ea4c11bf07508a0dee5a7fb0cb571264b
Ladislav Cicatko <cicatkolada@gmail.com>
2021-07-15 14:36:45 +0200

    zapracovani zmen

--- ukol_adresar.js
+++ ukol_adresar.js
@@ -64,7 +64,6 @@ app.delete('/delete_user/:Prijmeni',  function (req, res) {
 
 app.post('/vloz_user/:Jmeno/:Prijmeni/:Email/:Tel/:Ulice/:Mesto/:PSC',  function (req, res) {
   var kontakt = [];
-  res.send(req.body);
   const jmeno = req.params.Jmeno;
   const prijmeni = req.params.Prijmeni;
   const email = req.params.Email;
@@ -75,15 +74,20 @@ app.post('/vloz_user/:Jmeno/:Prijmeni/:Email/:Tel/:Ulice/:Mesto/:PSC',  function
   var MongoClient = require('mongodb').MongoClient;
   var url = "mongodb+srv://xcicat00:karamba81@cluster0.2uczv.mongodb.net/admin?replicaSet=atlas-1048qa-shard-0&readPreference=primary&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1&3t.uriVersion=3&3t.connection.name=Lada&3t.databases=admin,test&3t.alwaysShowAuthDB=true&3t.alwaysShowDBFromUserRole=true&3t.sslTlsVersion=TLS";
   MongoClient.connect(url, (err, db) => {
-    if (err) throw err;
-    var dbo = db.db("pokus");
-    //kontakt = { Jmeno: "Jan", Prijmeni: "Cermak", Email: "jancermal@gmail.cz", Tel:"733455788", Ulice:"Hlavni Trida 14",Mesto:"Benesov",PSC: "49001" };
-    kontakt = { Jmeno: jmeno, Prijmeni: prijmeni, Email: email, Tel: tel, Ulice: ulice, Mesto: mesto, PSC: psc };
-    dbo.collection("Kontakty").insertOne(kontakt,function(err,res){
-        if (err) throw err;
-        console.log("Zapsal jsi kontakt do kolekce Kontakty!");  
+    if (err) res.status(500).send("Chyba pripojeni na Databazi");
+    else {
+      var dbo = db.db("pokus");
+      //kontakt = { Jmeno: "Jan", Prijmeni: "Cermak", Email: "jancermal@gmail.cz", Tel:"733455788", Ulice:"Hlavni Trida 14",Mesto:"Benesov",PSC: "49001" };
+      kontakt = { Jmeno: jmeno, Prijmeni: prijmeni, Email: email, Tel: tel, Ulice: ulice, Mesto: mesto, PSC: psc };
+      dbo.collection("Kontakty").insertOne(kontakt,function(err){
         db.close();
+        if (err) res.status(500).send("Chyba pri vkladani kontaktu");
+        else{
+          
+          res.send("Zapsal jsi kontakt do kolekce Kontakty!");    
+        }
     });
+    }
   });  
 });
 
